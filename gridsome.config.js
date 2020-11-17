4077eb8fa6f94d17e483432c2eb1da5cbdb01c39
SIMON DODSON <simondodson@gmail.com>
2020-11-17 12:16:07 +0700

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
+  siteName: 'health", see na, zdrowie. ',
+  siteDescription: 'Literally "for health" or "to [your] health", see na, zdrowie. 2019 WIKIPEDIA ',
 
   templates: {
     Post: '/:title',
